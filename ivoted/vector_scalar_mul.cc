f21dd233fe401e77a3eb76716b8824b4b7683580
GiuseppeSorrentino99 <giuseppe.sorrentino@polimi.it>
2025-04-10 21:58:55 +0200

    useful comment

--- ivoted/vector_scalar_mul.cc
+++ ivoted/vector_scalar_mul.cc
@@ -19,7 +19,21 @@ extern "C" {
 void vector_scalar_mul_aie_scalar(int32_t *a, int32_t *c, int32_t *factor,
                                   int32_t N) {
   event0();
-  constexpr int vec_factor = 32;
+  /*
+  The size of the vector depends on the type. For example, the standard vector register in AIE2 is 512 bits.
+  For int16_t, that means we can store 32 of them in 1x 512b vector register.
+  Extending this to the other supported data types, we have the following abbreviated table:
+  Data type	Vector size
+    int32_t   	16
+    int16_t   	32
+    int8_t   	  64
+    int4_t   	  128
+
+  Note that if the listed data types * vector size ends up being larger than 512-bits,
+  that just means it's stored in 2+ vector registers instead of just one.
+  */
+
+  constexpr int vec_factor = 16; // try to increase this value and check performance :D - my best was 64
   int32_t *__restrict pA1 = a;
   int32_t *__restrict pC1 = c;
   const int F = N / vec_factor;
